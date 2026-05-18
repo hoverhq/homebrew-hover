9a27a3e554867919a3f4175f627ba9d57255fcd5
Nelson Melo <nmelo@nmelo.co>
2026-05-18 09:17:00 -0400

    Formula: bump hover to v0.1.5

--- Formula/hover.rb
+++ Formula/hover.rb
@@ -4,29 +4,29 @@
 class Hover < Formula
   desc "Drone relay CLI for Hover"
   homepage "https://hoverfeed.com"
-  version "0.1.4"
+  version "0.1.5"
 
   on_macos do
     on_arm do
       url "https://github.com/hoverhq/releases/releases/download/v#{version}/hover-darwin-arm64.tar.gz"
-      sha256 "e9e2fda05e129f06d9203503fe3a0ea468cf7ee737dd24a35a8a09c84b643dc8"
+      sha256 "f8cb9b0fadaa9a541aa69561d424e1f5813db10c9d5cabd2728addd8dd352de6"
     end
 
     on_intel do
       url "https://github.com/hoverhq/releases/releases/download/v#{version}/hover-darwin-amd64.tar.gz"
-      sha256 "d6814fa841fe5a8161a74b2c661eeb504d187036a0f30bc546cb66e8955ab22f"
+      sha256 "fec108aaa3cfdefa5560f0a2ec27ad9b18f06d1a8189e4ba7b087afb31933b89"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/hoverhq/releases/releases/download/v#{version}/hover-linux-amd64.tar.gz"
-      sha256 "0c41efa9c58e260bfaf593b7b34e087370b7c290dea9e18bb732ba8911f691e9"
+      sha256 "e6a1af3ac4a4e6c6d130ecab49d40f4ce2aa7e571448e2977c286272b4787908"
     end
 
     on_arm do
       url "https://github.com/hoverhq/releases/releases/download/v#{version}/hover-linux-arm64.tar.gz"
-      sha256 "835179309c8d86af63972b28add48d625c89ee905cae4c6ca121ef414a972830"
+      sha256 "d745fe987377e13ab11cdef395f55296cc76c38b3186395cdd4c518d04f54a7a"
     end
   end
 
